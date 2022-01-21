d198e1209646cce6be8ef14425f7cebf220ba70d
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-01-21 20:28:02 +0000

    Fix code & CI

--- src/database/models/UserData.ts
+++ src/database/models/UserData.ts
@@ -1,78 +1,55 @@
-import mongoose from "mongoose";
+export type UserServers = {
+    [key: number]: {
+        icon?: string;
+        name?: string;
+        id?: string;
+        description?: string;
+        poster?: string;
+        isOwner?: boolean;
+        members?: [
+            {
+                username?: string;
+                id?: string;
+                icon?: number;
+                status?: {
+                    icon?: "online" | "offline" | "idle" | "dnd";
+                    message?: string;
+                };
+            }
+        ];
+        categories?: [
+            {
+                name?: "uncategorized" | string;
+                id?: string;
+                members?: [
+                    {
+                        name?: string;
+                        type?: "text" | "voice" | "stage" | "announcement";
+                        id?: string;
+                        description?: string;
+                    }
+                ];
+            }
+        ];
+    };
+};
 
-export type Data = {
+export type UserData = {
     username?: string;
     name?: string;
     email?: string;
     phone?: string;
     avatar?: string;
-    servers?: [
-        {
-            icon?: string;
-            name?: string;
-            id?: number;
-            description?: string;
-            poster?: string;
-            isOwner?: boolean;
-            members?: [
-                {
-                    username?: string;
-                    icon?: string;
-                    id?: string;
-                    status?: {
-                        icon?: "online" | "offline" | "idle" | "dnd";
-                        message?: string;
-                    };
-                }
-            ];
-            categories?: [
-                {
-                    name?: "uncategorized" | string;
-                    id?: string;
-                    members?: [
-                        {
-                            name?: string;
-                            type?: "text" | "voice" | "stage" | "announcement";
-                            id?: string;
-                            description?: string;
-                        }
-                    ];
-                }
-            ];
-        }
-    ];
+    servers?: UserServers;
 };
 
-export const schema = new mongoose.Schema({
-    username: String,
-    name: String,
-    email: String,
-    phone: String,
-    avatar: String,
-    servers: [
-        {
-            icon: String,
-            name: String,
-            id: String,
-            description: String,
-            poster: String,
-            isOwner: Boolean,
-            members: [
-                {
-                    username: String,
-                    icon: String,
-                    id: Number,
-                    status: {
-                        icon: String,
-                        message: String,
-                    },
-                },
-            ],
-        },
-    ],
-});
-
-const UserData = mongoose.model("ODS_UserData", schema);
-console.log("[Model] Registered model: UserData.");
-
-export default UserData;
+export class UserSchema {
+    constructor(
+        public username?: string,
+        public name?: string,
+        public email?: string,
+        public phone?: string,
+        public avatar?: string,
+        public servers?: UserServers
+    ) {}
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import proxy from "express-http-proxy";
-import mongoose from "mongoose";
+import mongodb from "mongodb";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
@@ -20,54 +20,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-mongoose.connect(
-    "mongodb://localhost:27017/opendocs_testing",
-    {},
-    async (err) => {
-        if (err) {
-            console.log(err);
-            app.get("*", (req, res) => {
-                res.status(500);
-                res.type("text/plain");
-                res.send(
-                    "500 | Could not connect to database. Please try again later."
-                );
-            });
-        } else {
-            console.log("Connected to database!");
-            await import("./database/models/UserData");
-            io.on("connection", (socket) => {
-                console.log(
-                    `${socket.id} | Client connected!    | { id: ${socket.id}, ip: ${socket.handshake.address} }`
-                );
-                socket.on("request_data", () => {
-                    const id = "61ba64ed142ea93ba3343990";
-                    mongoose
-                        .model("ODS_UserData")
-                        .findById(id, (err: string, doc: string) => {
-                            if (err) return console.error(err);
-                            socket.emit("initialUserData", doc);
-                        });
-                });
-                socket.on("disconnect", () => {
-                    console.log(
-                        `${socket.id} | Client disconnected! | { id: ${socket.id}, ip: ${socket.handshake.address} }`
-                    );
-                });
-            });
-            // Routes
-            app.get("/api", api);
-            app.get("/api/*", api);
-            app.post("/api", api);
-            app.post("/api/*", api);
+const dbclient = new mongodb.MongoClient(
+    "mongodb://localhost:27017/opendocs_testing"
+);
 
-            app.get("/*", frontend);
-            app.post("/*", frontend);
-        }
-        servlet.listen(port, () => {
+async function main() {
+    servlet.listen(port, () => {
+        console.log(
+            `⚡️ [server] App listening on port ${port} (Frontend on port ${frontendPort}, API on port ${apiPort}).`
+        );
+    });
+    try {
+        await dbclient.connect();
+        console.log("Connected to database!");
+        await import("./database/models/UserData");
+        io.on("connection", (socket) => {
             console.log(
-                `⚡️ [server] App listening on port ${port} (Frontend on port ${frontendPort}, API on port ${apiPort}).`
+                `${socket.id} | Client connected!    | { id: ${socket.id}, ip: ${socket.handshake.address} }`
+            );
+            socket.on("request_data", () => {
+                // const id = "61ba64ed142ea93ba3343990";
+                socket.emit("initialUserData", {});
+            });
+            socket.on("disconnect", () => {
+                console.log(
+                    `${socket.id} | Client disconnected! | { id: ${socket.id}, ip: ${socket.handshake.address} }`
+                );
+            });
+        });
+        // Routes
+        app.get("/api", api);
+        app.get("/api/*", api);
+        app.post("/api", api);
+        app.post("/api/*", api);
+
+        app.get("/*", frontend);
+        app.post("/*", frontend);
+    } catch (err) {
+        console.log(err);
+        app.get("*", (req, res) => {
+            res.status(500);
+            res.type("text/plain");
+            res.send(
+                "500 | Could not connect to database. Please try again later."
             );
         });
+    } finally {
+        dbclient.close();
     }
-);
+}
+
+main();
